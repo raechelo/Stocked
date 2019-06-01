5b99fff5777853a8b451a5490d09a10ae59fec05
raechel <odom.raechel@gmail.com>
2019-06-01 11:35:54 -0600

    footer set up and positioned

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../Header/Header';
 import Results from '../../Containers/Results/Results';
 import Form from '../../Containers/Form/Form';
+import Footer from '../Footer/Footer';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <Header />
     <Form />
     <Results />
+    <Footer />
     </div>
   );
 }
