50ead6bff57047797cbd2f7ef6a8a6c39b690b53
raechel <odom.raechel@gmail.com>
2019-06-01 15:58:11 -0600

    app snapshot added

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,3 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from '../App/App';
+import { shallow } from 'enzyme';
+
+describe('App', () => {
+
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />)
+  });
+
+  it('should match the snapshot with all the data passed in', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+})
