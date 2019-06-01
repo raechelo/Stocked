d86e061bed0e9b6f0db4341cb4c1cb033f5a3b62
raechel <odom.raechel@gmail.com>
2019-06-01 12:17:00 -0600

    images spaced better in footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,9 +5,11 @@ export default function Footer() {
   return (
     <footer className="Footer">
       <h5>difficulty:
+        <span>
         <img alt="cleaver" src={cleaver} />
         <img alt="cleaver" src={cleaver} />
         <img alt="cleaver" src={cleaver} />
+        </span>
       </h5>
     </footer>
   )
