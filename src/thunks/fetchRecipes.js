73322392ab280d7628bb20e6a318d08a6ac63f57
raechel <odom.raechel@gmail.com>
2019-06-01 14:46:09 -0600

    thunk now dispatches action to fetch meal based on result id and resets information into store

--- src/thunks/fetchRecipes.js
+++ src/thunks/fetchRecipes.js
@@ -1,16 +1,21 @@
 import { fetchData } from '../api/fetchData';
+import { cleanRecipes } from '../api/cleanRecipes';
 
 import { isLoading, setError, setRecipes } from '../actions'; 
 
-export const fetchRecipes = url => {
+export const fetchRecipes = arr => {
   return async dispatch => {
     try {
       dispatch(isLoading(true));
-      const recipes = await fetchData(url);
-      // const recipes = cleanRecipes(data)
-      console.log(recipes)
+      let meals
+      const recipes = arr.map(async a => {
+        const { id } = a
+        meals = await fetchData(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+        console.log(meals)
+        cleanRecipes(meals)
+        dispatch(setRecipes(meals))
+      })
       dispatch(isLoading(false));
-      dispatch(setRecipes(recipes))
     } catch (error) {
       // dispatch(setError(error.message))
     }
