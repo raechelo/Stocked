6a88140808b486f8aa11b31b4bc174022068b37f
raechel <odom.raechel@gmail.com>
2019-06-02 02:36:51 -0600

    results reducer fully tested to current cases:

---
+++ src/reducers/__tests__/resultsReducer.test.js
@@ -0,0 +1,19 @@
+import { results } from '../resultsReducer';
+import * as actions from '../../actions';
+
+describe('results', () => {
+
+  it('should return the default state', () => {
+    const expected = [];
+    const result = results(undefined, []);
+
+    expect(result).toEqual(expected);
+  });
+
+  it('should return the state with all the results', () => {
+    const expected = [{name: 'Avocados'}];
+    const result = results([{name: 'Avocados'}], actions.setResults([{name: 'Avocados'}]));
+
+    expect(result).toEqual(expected);
+  })
+});
