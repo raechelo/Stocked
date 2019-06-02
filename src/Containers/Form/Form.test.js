966290273905787b526f3057724945f2bc0e9431
raechel <odom.raechel@gmail.com>
2019-06-02 12:03:36 -0600

    form tests add, dispatch not passing, unsure why

--- src/Containers/Form/Form.test.js
+++ src/Containers/Form/Form.test.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { Form } from './Form';
 import { fetchResults } from '../../thunks/fetchResults';
+import { fetchIngredients } from '../../thunks/fetchIngredients';
+
+import { mapStateToProps, mapDispatchToProps } from './Form';
 
 describe('Form', () => {
 
   jest.mock('../../thunks/fetchResults')
+  jest.mock('../../thunks/fetchIngredients')
 
   let wrapper;
   let ingredients = ['Mangoes', 'Jalapenos', 'Cilantro', 'Avocaods']
@@ -32,6 +36,44 @@ describe('Form', () => {
     wrapper.setState({ ingredientOne: 'bacon', ingredientTwo: 'mac', ingredientThree: 'cheese'});
     wrapper.instance().handleClick()
     expect(mockFetchResults).toHaveBeenCalled();
+  });  
+
+});
+
+describe('mapStateToProps', () => {
+  
+  it('should return an object with the ingredients array', () => {
+    const mockState = {
+      ingredients: [{name:'Avocados', id: 1}]
+    }
+    const expected = {
+      ingredients: [{name:'Avocados', id: 1}]
+    }
+
+    const mappedProps = mapStateToProps(mockState);
+    expect(mappedProps).toEqual(expected);
+  });
+
+});
+
+describe('mapDispatchToProps', () => {
+
+  it.skip('should call dispatch and fetch ingredients', () => {
+    const mockDispatch = jest.fn();
+    const actionToDispatch = fetchIngredients('www.placeholder.com');
+    mapDispatchToProps(mockDispatch);
+    
+    
+    expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+  });
+
+  it('should call dispatch and fetch results', () => {
+    const mockDispatch = jest.fn();
+    const actionToDispatch = fetchResults('www.placeholder.com');
+    const mappedProps = mapDispatchToProps(mockDispatch);
+    mappedProps.handleClick('www.placeholder.com');
+    
+    expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
   });
   
 });
