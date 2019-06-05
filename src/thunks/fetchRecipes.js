d983a1610787d1273251c43d7ac9327f3759a034
raechel <odom.raechel@gmail.com>
2019-06-04 21:14:37 -0600

    removal of cleanRecipe helper function for thunk to work effectively

--- src/thunks/fetchRecipes.js
+++ src/thunks/fetchRecipes.js
@@ -1,5 +1,4 @@
 import { fetchData } from '../api/fetchData';
-import { cleanRecipes } from '../api/cleanRecipes';
 
 import { isLoading, setError, setRecipes } from '../actions'; 
 
@@ -8,15 +7,13 @@ export const fetchRecipes = arr => {
     try {
       dispatch(isLoading(true));
       let meals = []
-      let recipes = arr.map(async a => {
+      await arr.map(async a => {
         const { id } = a
         await fetchData(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
         .then(res => meals.push(res.meals[0]))
       })
-      // meals = cleanRecipes(meals)
       dispatch(isLoading(false));
-      dispatch(setRecipes(meals))
-      // return Promise.all(recipes);
+      await dispatch(setRecipes(meals));
     } catch (error) {
       dispatch(setError(error.message))
     }
