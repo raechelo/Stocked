0473f553ee366850da16d4c16335b2e41414c3f3
raechel <odom.raechel@gmail.com>
2019-07-15 10:08:23 -0600

    Updates

---
+++ precache-manifest.ae3565f07b23b8d5e863bb37e21d40ee.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "772263c3b7ddd5a477235c98306a2222",
+    "url": "/stocked/index.html"
+  },
+  {
+    "revision": "f778c2b49562f8007ccd",
+    "url": "/stocked/static/css/2.d58fcd6a.chunk.css"
+  },
+  {
+    "revision": "7a8a298ce2eff4b21974",
+    "url": "/stocked/static/css/main.ecccfa04.chunk.css"
+  },
+  {
+    "revision": "f778c2b49562f8007ccd",
+    "url": "/stocked/static/js/2.9c3e0a88.chunk.js"
+  },
+  {
+    "revision": "7a8a298ce2eff4b21974",
+    "url": "/stocked/static/js/main.3293f605.chunk.js"
+  },
+  {
+    "revision": "b74788064784e9b04874",
+    "url": "/stocked/static/js/runtime~main.110d60f1.js"
+  },
+  {
+    "revision": "20db649091f3d3076bb6c69b43c007c2",
+    "url": "/stocked/static/media/cleaver.20db6490.svg"
+  },
+  {
+    "revision": "9dcb13877304bfb2da2a2d7e686288fc",
+    "url": "/stocked/static/media/cuttingboard.9dcb1387.png"
+  },
+  {
+    "revision": "abe742a97abe73d7cc48b2a4fc2b512e",
+    "url": "/stocked/static/media/food.abe742a9.jpeg"
+  },
+  {
+    "revision": "c05092b78ab5cd47568eea9af0bdc757",
+    "url": "/stocked/static/media/veggie.c05092b7.gif"
+  },
+  {
+    "revision": "a6372485318244c18a497ae9a9b62467",
+    "url": "/stocked/static/media/youtube.a6372485.svg"
+  }
+]);
